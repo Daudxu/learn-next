a2650ed6c4b6937795bc4e18bbb9b3646e88d284
xudan <875126243@qq.com>
2023-06-20 15:40:14 +0800

    fix update

---
+++ pages/(marketing)/test/page.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import React, { useState } from 'react';
+
+const Page = () => {
+  return (
+    <>
+      <Head>
+          <title>blog App</title>
+      </Head>
+      <div>
+        <h1>Page</h1>
+      </div>
+    </>
+  );
+}
+
+export default Page
